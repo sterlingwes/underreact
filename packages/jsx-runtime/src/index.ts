47263d6e75d4683c23433fd668198123628e1be4
Wes Johnson <swj@wesquire.ca>
2020-07-09 23:07:49 -0400

    fix: update runtime to improve dead code removal

--- packages/jsx-runtime/src/index.ts
+++ packages/jsx-runtime/src/index.ts
@@ -3,41 +3,36 @@ import { createElement } from './server';
 import { applyEventHandlers } from './client';
 
 const RUN_MODE_ENV = 'UR_RUN_MODE';
-const runModes: RunMode[] = ['dev', 'client', 'server'];
 
-type MaybeRunMode = string | undefined | RunMode;
+const validRunMode =
+  process.env.UR_RUN_MODE === 'server' ||
+  process.env.UR_RUN_MODE === 'client' ||
+  process.env.UR_RUN_MODE === 'dev';
 
-const validMode = (env: string | RunMode): env is RunMode =>
-  env === 'dev' || env === 'client' || env === 'server';
+if (!validRunMode) {
+  throw new Error(
+    `${RUN_MODE_ENV} env not set to one of dev|client|server. Unable to determine runtime mode for jsx runtime.`,
+  );
+}
 
-const runMode = (env: MaybeRunMode = process.env.UR_RUN_MODE): Maybe<RunMode> => {
-  if (typeof env !== 'string' || validMode(env) === false) {
-    throw new Error(
-      `${RUN_MODE_ENV} env not set to one of dev|client|server. Unable to determine runtime mode for jsx runtime.`,
-    );
-  }
-  return env as RunMode;
-};
+let jsxRuntime;
 
-const jsxRuntime = (def: ElementHierarchy): HTMLElement | JSXDefinitionHierarchy => {
-  let element;
-  switch (runMode()) {
-    case 'dev':
-      element = createElement(def);
-      // @ts-ignore
-      applyEventHandlers(def, element);
-      break;
-    case 'client':
-      // @ts-ignore
-      element = applyEventHandlers(def);
-      break;
-    case 'server':
-      element = createElement(def);
-      break;
-    default:
-      throw new Error(`Invalid run mode`);
-  }
-  return element;
-};
+if (process.env.UR_RUN_MODE === 'server') {
+  jsxRuntime = (def: ElementHierarchy): HTMLElement => createElement(def);
+}
+
+if (process.env.UR_RUN_MODE === 'client') {
+  // @ts-ignore
+  jsxRuntime = (def: ElementHierarchy): JSXDefinitionHierarchy => applyEventHandlers(def);
+}
+
+if (process.env.UR_RUN_MODE === 'dev') {
+  jsxRuntime = (def: ElementHierarchy): HTMLElement => {
+    const element = createElement(def);
+    // @ts-ignore
+    applyEventHandlers(def, element);
+    return element;
+  };
+}
 
 export default jsxRuntime;
